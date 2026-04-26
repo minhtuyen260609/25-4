e4b8d606ef8de6fabe6169f6236ee1b66e5a49f1
Minh Tuyen <email_github_cua_ban@example.com>
2026-04-26 20:20:33 +0700

    day1

--- src/main/java/com/example/_54/controller/TodoController.java
+++ src/main/java/com/example/_54/controller/TodoController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 public class TodoController {
 
@@ -16,31 +18,53 @@ public class TodoController {
         this.repo = repo;
     }
 
-    // 👉 Trang danh sách
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("todos", repo.findAll());
         return "index";
     }
 
-    // 👉 Mở form
     @GetMapping("/add")
-    public String showForm(Model model) {
+    public String showAddForm(Model model) {
         model.addAttribute("todo", new Todo());
         return "form";
     }
 
-    // 👉 Lưu
+    @GetMapping("/todos/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        Todo todo = repo.findById(id).orElse(null);
+
+        if (todo == null) {
+            redirectAttributes.addFlashAttribute("message", "Không tìm thấy Todo!");
+            return "redirect:/";
+        }
+
+        model.addAttribute("todo", todo);
+        return "form";
+    }
+
     @PostMapping("/save")
     public String save(
             @Valid @ModelAttribute("todo") Todo todo,
-            BindingResult result
+            BindingResult result,
+            RedirectAttributes redirectAttributes
     ) {
         if (result.hasErrors()) {
             return "form";
         }
-
         repo.save(todo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/todos/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            redirectAttributes.addFlashAttribute("message", "Xóa thành công!");
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Todo không tồn tại!");
+        }
+
         return "redirect:/";
     }
 }
